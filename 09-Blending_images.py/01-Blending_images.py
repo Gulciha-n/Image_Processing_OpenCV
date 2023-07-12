f43298489d493fd8da9e966e534038b2cd4f6588
Gülcihan <gulmezgulcihan44@gmail.com>
2023-07-12 06:20:56 +0300

    Blending_images

---
+++ 09-Blending_images.py/01-Blending_images.py
@@ -0,0 +1,45 @@
+import cv2
+import matplotlib.pyplot as plt
+
+#default color =BGR
+#return BGR to the RGB 
+img1 = cv2.imread("img1.JPG")
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+
+
+img2 = cv2.imread("img2.JPG")
+İMG2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+
+#
+plt.figure()
+plt.imshow(img1)
+
+plt.figure()
+plt.imshow(img2)
+
+#look at dimensions and resize to same size
+print(img1.shape)
+print(img2.shape)
+
+#resize
+img1 = cv2.resize(img1, (600,600))
+img2 = cv2.resize(img2, (600,600))
+
+plt.figure()
+plt.imshow(img1)
+
+plt.figure()
+plt.imshow(img2)
+
+
+# blended = alpha*img1 + beta*img2 + gamma
+blended = cv2.addWeighted(src1=img1, alpha=0.5, src2=img2, beta =0.5, gamma=0)
+plt.imshow(blended)
+
+
+
+
+
+
+
+
